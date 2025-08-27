4c9b5808a4c09a75fc05fb4c9e4918b174c5446e
mohamed <egyptgoschool@gmail.com>
2025-08-28 00:13:53 +0300

    Add Room Details Hotel

---
+++ src/components/Booking/Hotel/CardRoomDetailsHotel.tsx
@@ -0,0 +1,17 @@
+import ImgRoom from "@/assets/images/ImgRoom.png";
+
+function CardRoomDetailsHotel() {
+  return (
+    <div>
+      <div>
+        <img src={ImgRoom} alt="Hotel" />
+        <div>
+          {/* <img src={ImgRoom} alt="Hotel" /> */}
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default CardRoomDetailsHotel;
